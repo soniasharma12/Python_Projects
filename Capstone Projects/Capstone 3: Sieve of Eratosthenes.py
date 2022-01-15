9e07e053d6bd797fbc00a88df6629c9b7c689102
Sonia Sharma <71413895+soniasharma12@users.noreply.github.com>
2022-01-15 14:17:48 -0500

    Update Capstone 3: Sieve of Eratosthenes.py

--- Capstone Projects/Capstone 3: Sieve of Eratosthenes.py
+++ Capstone Projects/Capstone 3: Sieve of Eratosthenes.py
@@ -1,1 +1,15 @@
+n = int(input("Please enter a number till which you need all primes: "))
+primes = [i for i in range(n + 1)]
 
+print(primes)
+for prime in range(2, len(primes)):
+  current_prime = prime
+  if current_prime != 0: 
+    # Write your code from here.  
+    for i in range(current_prime * 2, len(primes), current_prime):
+      primes[i] = 0
+
+print(primes)
+primes = primes[2:]
+
+print(primes)
